78463dcf56b73cca950927e6f7388887e2514a09
zaqc <teamcoders@gmail.com>
2016-09-19 13:21:20 +0300

    add main menu

--- src/AScanWnd.cpp
+++ src/AScanWnd.cpp
@@ -15,6 +15,238 @@
 #include <SDL2/SDL2_gfxPrimitives.h>
 //----------------------------------------------------------------------------
 
+//============================================================================
+//	SDL Util
+//============================================================================
+SDL_Texture *CreateText(SDL_Renderer *aRnd, TTF_Font *aFont,
+		std::string aCaption, int aW, int aH, SDL_Color aColor,
+		SDL_Color aBkColor) {
+
+	SDL_Texture *res_txt = SDL_CreateTexture(aRnd, SDL_PIXELFORMAT_ABGR8888,
+			SDL_TEXTUREACCESS_TARGET, aW, aH);
+
+	SDL_SetRenderTarget(aRnd, res_txt);
+	SDL_SetRenderDrawColor(aRnd, 0, 0, 0, 0);
+	SDL_RenderClear(aRnd);
+	SDL_SetRenderDrawColor(aRnd, aBkColor.r, aBkColor.g, aBkColor.b,
+			aBkColor.a);
+	SDL_Rect r = { 2, 2, aW - 4, aH - 4 };
+	SDL_RenderFillRect(aRnd, &r);
+
+	SDL_Surface *surf = TTF_RenderText_Blended(aFont, aCaption.c_str(), aColor);
+
+	SDL_Texture *txt = SDL_CreateTextureFromSurface(aRnd, surf);
+
+	int sx = 0;
+	int sy = 0;
+	int sw = surf->w;
+	int sh = surf->h;
+	int dx = 3;
+	int dy = 3;
+	int dw = aW - 6;
+	int dh = aH - 6;
+	if (sh > dh) {
+		sy += (sh - dh) / 2;
+		sh = dh;
+	} else if (sh < dh) {
+		dy += (dh - sh) / 2;
+		dh = sh;
+	}
+
+	if (sw > dw) {
+		sx += (sw - dw) / 2;
+		sw = dw;
+	} else if (sw < dw) {
+		dx += (dw - sw) / 2;
+		dw = sw;
+	}
+
+	SDL_Rect src_rect = (SDL_Rect ) { sx, sy, sw, sh };
+	SDL_Rect dst_rect = (SDL_Rect ) { dx, dy, dw, dh };
+	SDL_RenderCopy(aRnd, txt, &src_rect, &dst_rect);
+
+	SDL_SetRenderTarget(aRnd, NULL);
+
+	return res_txt;
+}
+//----------------------------------------------------------------------------
+
+//============================================================================
+//	MenuItem
+//============================================================================
+MenuItem::MenuItem(Menu *aMenu, std::string aCaption, int aID) {
+	m_Menu = aMenu;
+	m_Caption = aCaption;
+	m_ID = aID;
+	m_SubMenu = NULL;
+	m_Txt = NULL;
+	m_Focus = false;
+	m_Invalidate = false;
+	m_W = m_H = 0;
+}
+
+MenuItem::MenuItem(Menu *aMenu, std::string aCaption, Menu *aSubMenu) {
+	m_Menu = aMenu;
+	m_Caption = aCaption;
+	m_ID = -1;
+	m_SubMenu = aSubMenu;
+	m_Txt = NULL;
+	m_Focus = false;
+	m_Invalidate = false;
+	m_W = m_H = 0;
+}
+
+MenuItem::~MenuItem(void) {
+
+}
+
+void MenuItem::SetFocus(bool aFocus) {
+
+}
+
+void MenuItem::Render(SDL_Renderer *aRnd, int aW, int aH) {
+	if (m_Txt != NULL)
+		SDL_DestroyTexture(m_Txt);
+	m_Txt = SDL_CreateTexture(aRnd, SDL_PIXELFORMAT_ABGR8888,
+			SDL_TEXTUREACCESS_TARGET, aW, aH);
+	m_W = aW;
+	m_H = aH;
+
+	SDL_SetRenderTarget(aRnd, m_Txt);
+	SDL_Color c = m_Menu->GetItemBackground();
+	SDL_SetRenderDrawColor(aRnd, 0, 0, 0, 0);
+	SDL_RenderClear(aRnd);
+	SDL_SetRenderDrawColor(aRnd, c.r, c.g, c.b, c.a);
+	SDL_Rect r = { 2, 2, aW - 4, aH - 4 };
+	SDL_RenderFillRect(aRnd, &r);
+
+	TTF_Font *font = m_Menu->GetItemFont();
+	SDL_Surface *surf = TTF_RenderText_Blended(font, m_Caption.c_str(),
+			m_Menu->GetItemColor());
+
+	SDL_Texture *txt = SDL_CreateTextureFromSurface(aRnd, surf);
+
+	int sx = 0;
+	int sy = 0;
+	int sw = surf->w;
+	int sh = surf->h;
+	int dx = 3;
+	int dy = 3;
+	int dw = m_W - 6;
+	int dh = m_H - 6;
+	if (sh > dh) {
+		sy += (sh - dh) / 2;
+		sh = dh;
+	} else if (sh < dh) {
+		dy += (dh - sh) / 2;
+		dh = sh;
+	}
+
+	if (sw > dw) {
+		sx += (sw - dw) / 2;
+		sw = dw;
+	} else if (sw < dw) {
+		dx += (dw - sw) / 2;
+		dw = sw;
+	}
+
+	SDL_Rect src_rect = (SDL_Rect ) { sx, sy, sw, sh };
+	SDL_Rect dst_rect = (SDL_Rect ) { dx, dy, dw, dh };
+	SDL_RenderCopy(aRnd, txt, &src_rect, &dst_rect);
+
+	SDL_SetRenderTarget(aRnd, NULL);
+}
+
+void MenuItem::Paint(SDL_Renderer *aRnd, int aX, int aY) {
+
+}
+
+//============================================================================
+//
+//============================================================================
+Menu::Menu(int aX, int aY, int aW, int aH, std::string aCaption,
+		Menu *aParent) {
+	m_X = aX;
+	m_Y = aY;
+	m_W = aW;
+	m_H = aH;
+
+	m_Caprion = aCaption;
+	m_Parent = aParent;
+
+	m_CaptionTxt = NULL;
+
+	if (NULL == aParent) {
+		m_ItemFont = TTF_OpenFont(
+				"/usr/share/fonts/truetype/freefont/FreeSerif.ttf", 24);
+		m_ItemColor = (SDL_Color ) { 255, 255, 0, 255 };
+		m_ItemBackground = (SDL_Color ) { 32, 32, 32, 192 };
+	} else {
+		m_ItemFont = NULL;
+	}
+}
+
+Menu::~Menu() {
+	if (NULL != m_ItemFont) {
+		TTF_CloseFont(m_ItemFont);
+		m_ItemFont = NULL;
+	}
+}
+
+TTF_Font *Menu::GetItemFont(void) {
+	if (NULL != m_Parent)
+		return m_Parent->GetItemFont();
+	return m_ItemFont;
+}
+
+SDL_Color Menu::GetItemColor(void) {
+	if (NULL != m_Parent)
+		return m_Parent->GetItemColor();
+	return m_ItemColor;
+}
+
+SDL_Color Menu::GetItemBackground(void) {
+	if (NULL != m_Parent)
+		return m_Parent->GetItemBackground();
+	return m_ItemBackground;
+}
+
+void Menu::AddMenuItem(std::string aCaption, int aID) {
+	MenuItem *mi = new MenuItem(this, aCaption, aID);
+	m_Items.push_back(mi);
+}
+
+void Menu::AddMenuItem(std::string aCaption, Menu *aSubMenu) {
+	MenuItem *mi = new MenuItem(this, aCaption, aSubMenu);
+	m_Items.push_back(mi);
+}
+
+void Menu::Render(SDL_Renderer *aRnd) {
+
+	if (NULL == m_CaptionTxt) {
+		m_CaptionTxt = CreateText(aRnd, GetItemFont(), m_Caprion, m_W, 32,
+				GetItemColor(), GetItemBackground());
+	}
+
+	SDL_Rect r = { m_X, m_Y, m_W, m_H };
+	SDL_SetRenderDrawBlendMode(aRnd, SDL_BLENDMODE_BLEND);
+	SDL_SetRenderDrawColor(aRnd, 32, 32, 32, 192);
+	SDL_RenderFillRect(aRnd, &r);
+	SDL_SetRenderDrawBlendMode(aRnd, SDL_BLENDMODE_NONE);
+
+	SDL_Rect dst_rect = { m_X, m_Y, m_W, 32 };
+	SDL_RenderCopy(aRnd, m_CaptionTxt, NULL, &dst_rect);
+
+}
+
+bool Menu::ProcessEvent(SDL_Event aEvent) {
+
+	return false;
+}
+
+//============================================================================
+//
+//============================================================================
 AScanWnd::AScanWnd(SDL_Renderer *aRnd, int aX, int aY, int aW, int aH) :
 		Window(aRnd, aX, aY, aW, aH) {
 	m_LAmpOne = NULL;
@@ -29,6 +261,9 @@ void AScanWnd::Init(void) {
 	AddControl(m_LAmpOne);
 	m_TBAmpOne = new TrackBar(10, 32, 64, 400);
 	AddControl(m_TBAmpOne);
+
+	Menu *mm = new Menu(100, 100, 200, 340, "Main Menu", NULL);
+	AddControl(mm);
 }
 
 void AScanWnd::Done(void) {
@@ -58,6 +293,6 @@ void AScanWnd::Paint(void) {
 		SDL_RenderDrawLine(m_Rnd, m_X, y, m_X + m_W, y);
 	}
 
-	roundedBoxRGBA(m_Rnd, 100, 100, 220, 140, 4, 255, 0, 0, 150);
-	stringRGBA(m_Rnd, 110, 110, "asdasdasd", 255, 250, 20, 255);
+	roundedBoxRGBA(m_Rnd, 100, 40, 220, 80, 4, 255, 0, 0, 150);
+	stringRGBA(m_Rnd, 110, 50, "asdasdasd", 255, 250, 20, 255);
 }
--- src/AScanWnd.h
+++ src/AScanWnd.h
@@ -12,20 +12,68 @@
 
 #include "Label.h"
 #include "TrackBar.h"
+//----------------------------------------------------------------------------
 
-class MenuItem{
+class Menu;
+//----------------------------------------------------------------------------
+
+SDL_Texture *CreateText(SDL_Renderer *aRnd, TTF_Font *aFont, std::string aCaption, int aW, int aH, SDL_Color aColor, SDL_Color aBkColor);
+//----------------------------------------------------------------------------
+
+class MenuItem {
+protected:
+	Menu *m_Menu;
+	std::string m_Caption;
+	int m_ID;
+	Menu* m_SubMenu;
+	SDL_Texture *m_Txt;
+	bool m_Focus;
+	bool m_Invalidate;
+	int m_H;
+	int m_W;
 public:
+	MenuItem(Menu *aMenu, std::string aCaption, int aID);
+	MenuItem(Menu *aMenu, std::string aCaption, Menu *aSubMenu);
+	virtual ~MenuItem(void);
+
+	void SetFocus(bool aFocus);
+
+	void Render(SDL_Renderer *aRnd, int aW, int aH);
+	void Paint(SDL_Renderer *aRnd, int aX, int aY);
 };
+//----------------------------------------------------------------------------
 
 class Menu: public Control {
 protected:
+	int m_X;
+	int m_Y;
+	int m_W;
+	int m_H;
 	std::string m_Caprion;
-	MenuItem *m_Parent;
+	Menu *m_Parent;
 	std::vector<MenuItem*> m_Items;
+
+	SDL_Color m_ItemColor;
+	SDL_Color m_ItemBackground;
+	TTF_Font *m_ItemFont;
+
+	SDL_Texture *m_CaptionTxt;
 public:
-	Menu(int aX, int aY, int aW, int aH, std::string aCaption, MenuItem *aParent);
+	Menu(int aX, int aY, int aW, int aH, std::string aCaption,
+			Menu *aParent);
 	virtual ~Menu();
+
+	TTF_Font *GetItemFont(void);
+	SDL_Color GetItemColor(void);
+	SDL_Color GetItemBackground(void);
+
+	void AddMenuItem(std::string aCaption, int aID);
+	void AddMenuItem(std::string aCaption, Menu *aSubMenu);
+
+	virtual void Render(SDL_Renderer *aRnd);
+	virtual bool ProcessEvent(SDL_Event aEvent);
 };
+//----------------------------------------------------------------------------
 
 class AScanWnd: public Window {
 protected:
@@ -42,5 +90,6 @@ public:
 
 	virtual void Paint(void);
 };
+//----------------------------------------------------------------------------
 
 #endif /* ASCANWND_H_ */
